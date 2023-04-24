14503bf8acb904317fa11ae7efc23c2bbac2293a
linogaliana <lino.galiana@insee.fr>
2023-04-24 09:27:02 +0000

    barcode scanner

---
+++ init.sh
@@ -0,0 +1,2 @@
+sudo apt-get install libzbar0
+sudo apt-get install ffmpeg libsm6 libxext6  -y
---
+++ main.py
@@ -0,0 +1,33 @@
+import cv2
+from pyzbar import pyzbar
+from skimage import io
+
+#url = "https://barcode-list.com/barcodeImage.php?barcode=5000112602999"
+url = 'barcode4.png'
+
+img = io.imread(url)
+
+def draw_barcode(decoded, image):
+    # n_points = len(decoded.polygon)
+    # for i in range(n_points):
+    #     image = cv2.line(image, decoded.polygon[i], decoded.polygon[(i+1) % n_points], color=(0, 255, 0), thickness=5)
+    # uncomment above and comment below if you want to draw a polygon and not a rectangle
+    image = cv2.rectangle(image, (decoded.rect.left, decoded.rect.top), 
+                            (decoded.rect.left + decoded.rect.width, decoded.rect.top + decoded.rect.height),
+                            color=(0, 255, 0),
+                            thickness=5)
+    return image
+
+# option 2
+decoded_objects = pyzbar.decode(img)
+
+for obj in decoded_objects:
+        # draw the barcode
+        print("detected barcode:", obj)
+        image = draw_barcode(obj, image)
+        # print barcode type & data
+        print("Type:", obj.type)
+        print("Data:", obj.data)
+        print()
+
+
