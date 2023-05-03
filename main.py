fde8b35593ce6802ba43daa3f8a99f40635ac22f
linogaliana <lino.galiana@insee.fr>
2023-05-03 16:49:57 +0000

    première version app fonctionnelle

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import streamlit as st
 from detect_barcode import extract_ean, visualise_barcode
 
-st.title('Détection de code barres')
+st.title('Hello streamlit')
 
-input_url = st.text_input('Renseigner un url', '')
+#input_url = st.text_input('Renseigner un url', 'https://barcode-list.com/barcodeImage.php?barcode=3274080005003')
 
+with st.sidebar:
+    input_url = st.file_uploader("Uploaded une photo:", accept_multiple_files=False)
+    st.image(input_url)
 
-st.image(input_url)
 
 decoded_objects = extract_ean(input_url)
 ean = decoded_objects[0].data.decode("utf-8")
 
 
-st.write('Detecter EAN:', ean)
+st.write('EAN détecté:', ean)
 
 # partie 2: retrouver le produit depuis openfood
 
@@ -36,10 +38,17 @@ def load_data():
 
 openfood_data = load_data()
 
-st.button("Rerun")
-
 subset = openfood_data.loc[openfood_data["code"] == ean]
 
-st.write('Consulter ce produit sur le site openfoodfacts:', subset["url"])
+st.write('Consulter ce produit sur le site openfoodfacts:', subset["url"].iloc[0])
+st.image(subset["image_url"].iloc[0])
 
-subset2 = subset.loc[:, ['code','']]
+
+info_nutritionnelles = [
+    'energy-kcal_100g', 'fat_100g', 'saturated-fat_100g',
+    'carbohydrates_100g', 'sugars_100g',
+    'proteins_100g', 'salt_100g']
+
+subset2 = subset.loc[:, ['code', 'product_name', 'category'] + info_nutritionnelles]
+st.dataframe(subset2)
+
