3bea19ab42e6cb5652faf7c1d448ba874571deca
linogaliana <lino.galiana@insee.fr>
2023-05-03 08:55:21 +0000

    remodularise

--- 1_retrieve_openfood.py
+++ 1_retrieve_openfood.py
@@ -38,5 +38,27 @@ openfood = pd.read_csv(
 
 openfood["code"] = openfood["code"].astype(str)
 
+
+
+# CATEGORIZATION OPENFOOD ---------------------------
+
+# Approche 1: binaire fasttext
+from download_pb import download_pb
+
+LOCATION_FASTTEXT_COICOP = "https://minio.lab.sspcloud.fr/projet-funathon/2023/sujet4/diffusion/model_coicop10.bin"
+
+# methode 1: lien url
+download_pb(url = LOCATION_FASTTEXT_COICOP, fname = "fasttext_coicop.bin")
+
+# methode 2: s3fs
+fs.download(LOCATION_FASTTEXT_COICOP.replace("https://minio.lab.sspcloud.fr/", ""), "fasttext_coicop.bin")
+
+download_pb("https://raw.githubusercontent.com/InseeFrLab/predicat/master/app/utils_ddc.py", "utils_ddc.py")
+
+from utils_ddc import preprocess_text
+
+openfood['preprocessed_labels'] = openfood['product_name'].str.upper().astype(str).apply(preprocess_text)
+
+
 with fs.open(DESTINATION_OPENFOOD, "wb") as f:
     openfood.to_parquet(f)
