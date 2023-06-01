c9134ffe059d0329a7d0400704a13bf82f16f723
linogaliana <lino.galiana@insee.fr>
2023-06-01 15:20:01 +0000

    nouvelles fonctions

--- main.py
+++ main.py
@@ -5,42 +5,65 @@ import requests
 import pandas as pd
 import s3fs
 
-from detect_barcode import extract_ean, visualise_barcode
+from utils.detect_barcode import extract_ean, visualise_barcode
+from utils.pipeline import find_product_openfood
+from utils.construct_figures import plot_product_info
 
 info_nutritionnelles = [
     'energy-kcal_100g', 'fat_100g', 'saturated-fat_100g',
     'carbohydrates_100g', 'sugars_100g',
     'proteins_100g', 'salt_100g']
 
-st.title('Hello streamlit')
+st.title('Mon Yuka 🥕 avec Python 🐍')
 
 #input_url = st.text_input('Renseigner un url', 'https://barcode-list.com/barcodeImage.php?barcode=3274080005003')
+def label_grade_formatter(s):
+    return s.split("_", maxsplit = 1)[0].capitalize()
+
+
+
 
 with st.sidebar:
-    input_url = st.file_uploader("Uploaded une photo:", accept_multiple_files=False)
+    input_method = st.radio(
+        "What\'s your favorite movie genre",
+        ('Photo enregistrée', 'Capture de la webcam'))
+    if input_method == 'Photo enregistrée':
+        input_url = st.file_uploader("Uploaded une photo:", accept_multiple_files=False)
+    else:
+        input_url = st.camera_input("Take a picture")
     if input_url is not None:
         img = visualise_barcode(input_url)
         cv2.imwrite('barcode_opencv.jpg', img)
         st.image('barcode_opencv.jpg')
+    options = st.multiselect(
+        'Quelles statistiques afficher ?',
+        ["nutriscore_grade", "nova_group", "ecoscore_grade"],
+        ["nutriscore_grade", "nova_group", "ecoscore_grade"],
+        format_func = label_grade_formatter)
+
 
 if input_url is not None:
     decoded_objects = extract_ean(input_url)
     ean = decoded_objects[0].data.decode("utf-8")
     st.write('EAN détecté:', ean)
-else:
-    st.write("Fournir une image")
+
+
+st.write(
+    '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">',
+    unsafe_allow_html=True
+)
+
 # partie 2: retrouver le produit depuis openfood
 
 
 
-
-URL_PARQUET = "https://minio.lab.sspcloud.fr/projet-funathon/2023/sujet4/diffusion/openfood.parquet"
+stats_notes = pd.read_parquet("https://minio.lab.sspcloud.fr/projet-funathon/2023/sujet4/diffusion/stats_notes_pandas.parquet")
 
 
 @st.cache_data
 def load_data(ean):
     openfood_data = find_product_openfood(ean)
-    return openfood_data.df()
+    return openfood_data
 
 if input_url is not None:
     subset = load_data(ean)
@@ -51,9 +74,10 @@ else:
 st.write('Consulter ce produit sur le site openfoodfacts:', subset["url"].iloc[0])
 st.image(subset["image_url"].iloc[0])
 
+st.dataframe(subset)
 
 
-
-subset2 = subset.loc[:, ['code', 'product_name', 'category'] + info_nutritionnelles]
-st.dataframe(subset2)
+for var in options:
+    fig = plot_product_info(subset, var, stats_notes)
+    st.plotly_chart(fig, height=800)
 
---
+++ utils/pipeline.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+
+info_nutritionnelles = [
+    'energy-kcal_100g', 'fat_100g', 'saturated-fat_100g',
+    'carbohydrates_100g', 'sugars_100g',
+    'proteins_100g', 'salt_100g']
+indices_synthetiques = [
+    "nutriscore_grade", "ecoscore_grade", "nova_group"
+]
+principales_infos = ['product_name', 'code', 'preprocessed_labels', 'coicop', 'url', 'image_url']
+liste_colonnes = principales_infos + indices_synthetiques + info_nutritionnelles
+liste_colonnes_sql = [f"\"{s}\"" for s in liste_colonnes]
+liste_colonnes_sql = ', '.join(liste_colonnes_sql)
+
+import duckdb
+con = duckdb.connect(database=':memory:')
+con.execute("""
+    INSTALL httpfs;
+    LOAD httpfs;
+    SET s3_endpoint='minio.lab.sspcloud.fr'
+""")
+
+url_data = "https://projet-funathon.minio.lab.sspcloud.fr/2023/sujet4/diffusion/openfood.parquet"
+
+
+def fuzzy_matching_product(openfood_produit, product_name, con, url_data):
+    out_textual = con.sql(
+        f"SELECT {liste_colonnes_sql} from read_parquet('{url_data}') WHERE jaro_winkler_similarity('{product_name}',product_name) > 0.9 AND \"energy-kcal_100g\" IS NOT NULL"
+    )
+    out_textual = out_textual.df()
+
+    out_textual.loc[:, indices_synthetiques] = pd.concat(
+        [clean_note(out_textual, s, "wide") for s in indices_synthetiques],
+        axis = 1
+    )
+
+    out_textual_imputed = pd.concat(
+        [
+            openfood_produit.loc[:, principales_infos].reset_index(drop = True),
+            pd.DataFrame(out_textual.loc[:, info_nutritionnelles].mean()).T,
+            pd.DataFrame(out_textual.loc[:, indices_synthetiques].replace('',np.nan).mode())
+        ], ignore_index=True, axis=1
+    )
+    out_textual_imputed.columns = principales_infos + info_nutritionnelles + indices_synthetiques
+    
+    return out_textual_imputed
+
+
+def find_product_openfood(ean):
+    openfood_produit = con.sql(
+        f"SELECT {liste_colonnes_sql} FROM read_parquet('{url_data}') WHERE CAST(ltrim(code, '0') AS STRING) = CAST(ltrim({ean}) AS STRING)"
+    ).df()
+    
+    product_name = openfood_produit["product_name"].iloc[0]
+    
+    if openfood_produit['energy-kcal_100g'].isnull().iloc[0]:
+        openfood_produit = fuzzy_matching_product(
+            openfood_produit, product_name, con, url_data
+        )
+    else:
+        openfood_produit.loc[:, indices_synthetiques] = pd.concat(
+            [clean_note(openfood_produit, s, "wide") for s in indices_synthetiques],
+            axis = 1
+        )        
+
+    return openfood_produit
+
+
+
+def clean_note(data, yvar = "note", format = "long"):
+    stats_one_variable = data.copy()
+    stats_one_variable[yvar] = stats_one_variable[yvar].astype(str)
+    if format == "long":
+        stats_one_variable = stats_one_variable.loc[~stats_one_variable[yvar].isin(['unknown','not-applicable'])]
+        stats_one_variable = stats_one_variable.dropna().drop_duplicates(subset = ['variable', yvar, 'coicop'])
+        stats_one_variable.loc[stats_one_variable['variable'] == "nova_group", yvar] = (
+                stats_one_variable.loc[stats_one_variable['variable'] == "nova_group", yvar]
+                .astype(float).astype("int64", errors = "ignore").apply(lambda d: chr(d + 64))
+            )
+    if format == "wide" :
+        stats_one_variable[yvar] = stats_one_variable[yvar].replace(r'(unknown|not-applicable)', '', regex = True)
+        if yvar == "nova_group":
+            stats_one_variable[yvar] = stats_one_variable[yvar].replace(r'nan', '', regex = True)
+            stats_one_variable.loc[stats_one_variable[yvar] == "", yvar] = 0 #hack to handle nan in conversion
+            stats_one_variable[yvar] = (stats_one_variable[yvar]
+                                        .astype(float)
+                                        .astype("int64", errors = "ignore").apply(lambda d: chr(d + 64))
+                                       )
+
+
+
+    stats_one_variable[yvar] = stats_one_variable[yvar].str.upper()
+    stats_one_variable[yvar] = stats_one_variable[yvar].astype(str)
+    stats_one_variable[yvar] = stats_one_variable[yvar].replace(r'NAN', '')
+    stats_one_variable[yvar] = stats_one_variable[yvar].replace(r'@', '')
+
+    if format == "wide":
+        return stats_one_variable[yvar]
+    return stats_one_variable
+
